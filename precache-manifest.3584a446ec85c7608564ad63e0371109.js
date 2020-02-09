9921347c42e464d16bdbe91b1e5efdf07be3c319
Kseniia <kuvaldina.ks@gmail.com>
2020-02-09 16:40:36 -0700

    Updates

---
+++ precache-manifest.3584a446ec85c7608564ad63e0371109.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1209972c43043c8d7ef84b7fbe1d1b64",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "7758569c8db7d4d2432c",
+    "url": "/weather/static/css/main.d14ba8c5.chunk.css"
+  },
+  {
+    "revision": "40946a4040558fec7464",
+    "url": "/weather/static/js/2.3df5b4bb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather/static/js/2.3df5b4bb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7758569c8db7d4d2432c",
+    "url": "/weather/static/js/main.5c1adf01.chunk.js"
+  },
+  {
+    "revision": "c99e76bcf2bdae1a8b8a",
+    "url": "/weather/static/js/runtime-main.953fba7f.js"
+  }
+]);
